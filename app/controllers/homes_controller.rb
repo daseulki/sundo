5ff2c56672b9672ab55500a6715a99eb9ad66e60
daseulki <sisial1002@gmail.com>
2018-01-18 17:18:41 +0900

    index mt-fix

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,12 @@
 class HomesController < ApplicationController
   def index
-    
+
+  end
+
+  def guide
+
+  end
+
+  def contact
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20180117092902) do
     t.string   "postimage"
     t.text     "content"
     t.string   "boardtype"
-    t.string   "category"
+    t.integer  "category"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -57,6 +57,9 @@ ActiveRecord::Schema.define(version: 20180117092902) do
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
+    t.string   "nickname"
+    t.string   "name"
+    t.string   "address"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -67,9 +70,6 @@ ActiveRecord::Schema.define(version: 20180117092902) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "nickname"
-    t.string   "name"
-    t.string   "address"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
